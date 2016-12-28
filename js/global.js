1e2a297dab889928712e6a14447d5bd676d25ce9
disastrid <no_u@fuckyou.computer>
2016-12-28 21:38:09 +0000

    finished for now.

--- js/global.js
+++ js/global.js
@@ -1,3 +1,4 @@
+// accordion menu
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -9,21 +10,15 @@ for (i = 0; i < acc.length; i++) {
 }
 
 // Number incrementor
-
 $(".number").on("click", function() {
-
 	var idToFind = $(this).attr("name");
 	var isDisabled = $('#' + idToFind).prop('disabled');
-
 	if (!isDisabled) {
-
 	var step = parseFloat($('#' + idToFind).attr('step'))
 	var value = parseFloat($('#' + idToFind).attr('value'));
 	console.log(value);
 	var thisMin = parseFloat($('#' + idToFind).attr('min'));
 	var thisMax = parseFloat($('#' + idToFind).attr('max'));
-
-
 		if ($(this).hasClass('decrement')) {
 			if ((value - step) >= thisMin) {
 				value = value - step;
@@ -39,38 +34,17 @@ $(".number").on("click", function() {
 	}
 });
 
+
+// File drop down
 $('.file-dropdown').on('click', function() {
 	var targetName = $(this).attr('name');
-	console.log(targetName); // this is showing we have the correct reference
-	// get the element with the id of targetNmae, and toggle the class "show"
 	$('#' + targetName).toggleClass('show');
 	var dropdownContent = $('#' + targetName);
 	var i;
 	for (i = 0; i < dropdownContent.length; i++) {
 		var openDropdown = dropdownContent[i];
 	}
+	$('#' + targetName).mouseleave(function() {
+		$(this).removeClass('show');
+	});
 });
-
-// // Dropdown menu 
-// function openDropdown(idName) {
-//   $('div').find('#' + idName).toggleClass('show');
-// }
-
-// function openMiniDropdown(idName) {
-//   $('div').find('#' + idName).addClass('show');
-// }
-
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
