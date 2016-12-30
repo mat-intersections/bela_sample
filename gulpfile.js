ce202f8689e6a321b1608df707181fe5b4e1d46c
disastrid <no_u@fuckyou.computer>
2016-12-30 01:19:09 +0000

    Some changes made for testing.

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ gulp.task('styles', function() {
     .pipe(concat('patternStyle.css'))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
     .pipe(gulp.dest('styles/'));
+
 });
 
 gulp.task('default', function() {
