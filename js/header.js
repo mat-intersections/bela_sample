df39fcb78a8e04e79a6e4e5809be1692b9f5742e
disastrid <no_u@fuckyou.computer>
2016-12-30 01:20:44 +0000

    header.js contains a little bit of functionality for the nav menu. Possibly to be rolled out for the IDE if necessary, but it currently isn't.

--- js/header.js
+++ js/header.js
@@ -1,8 +1,12 @@
-$(window).scroll(function(){
-    if($(document).scrollTop() > 125)
-    {
-        $('#hero').addClass('shrink');
-    } else {
-            $('#hero').removeClass('shrink');
-    }
+$(".sidebar > a").on("click", function() {
+	if ($(this).hasClass('selected')) {
+		$(this).removeClass('selected');
+	} else {
+	$('#main > .sidebar').find('> a').removeClass('selected');
+	$('#main > .sidebar').find('> span').removeClass('visible');
+		$(this).toggleClass('selected');
+	}
+	var sectionName = $(this).attr('href').substring(1);
+	$('.' + sectionName + '_section').toggleClass('visible');
+
 });
